7d743a748ad4f6021a1d329f36442ec5ddd940cc
Lucas Oliveira <lucaoxt@gmail.com>
2023-01-24 23:21:27 -0300

    implementation of sql logic of model LegalPerson.

--- src/model/LegalPerson.ts
+++ src/model/LegalPerson.ts
@@ -1,4 +1,13 @@
+import QueryBuilder from '../util/QueryBuilder';
+import Database from '../util/database';
+import Address from './Address';
+import City from './City';
 import Contact from './Contact';
+import State from './State';
+
+interface IFields {
+  id?: number;
+}
 
 export default class LegalPerson {
   private _id: number;
@@ -94,5 +103,123 @@ export default class LegalPerson {
     if (resultado.toString()[0] !== digitos[1]) return false;
 
     return true;
+  }
+
+  async save(): Promise<number> {
+    if (
+      this._id < 0 ||
+      this._corporateName.trim().length <= 0 ||
+      this._fantasyName.trim().length <= 0 ||
+      this._cnpj.trim().length < 18 ||
+      this._contact.id === 0
+    )
+      return -5;
+
+    let result = 0;
+    const parameters =
+      this._id > 0
+        ? [this._corporateName, this._fantasyName, this._id]
+        : [this._corporateName, this._fantasyName, this._cnpj, this._contact.id];
+
+    const query =
+      this._id > 0
+        ? new QueryBuilder()
+            .update('pessoa_juridica')
+            .set('pj_razao_social = ?,pj_nome_fantasia = ?')
+            .where('pj_id = ?')
+            .build()
+        : new QueryBuilder()
+            .insert(
+              'pessoa_juridica',
+              'pj_razao_social,pj_nome_fantasia,pj_cnpj,ctt_id',
+              '?,?,?,?',
+            )
+            .build();
+
+    result =
+      this._id > 0
+        ? await Database.instance.update(query, parameters)
+        : await Database.instance.insert(query, parameters);
+
+    return result;
+  }
+
+  async delete(): Promise<number> {
+    if (this._id <= 0) return -5;
+
+    let result = 0;
+    const parameters = [this._id];
+
+    const query = new QueryBuilder().delete('pessoa_juridica').where('pj_id = ?').build();
+
+    result = await Database.instance.delete(query, parameters);
+
+    return result;
+  }
+
+  async get(fields?: IFields): Promise<LegalPerson[]> {
+    const persons: LegalPerson[] = [];
+    const parameters = [];
+
+    let builder = new QueryBuilder()
+      .select(
+        `e.est_id, e.est_nome, e.est_sigla,
+         c.cid_id, c.cid_nome,
+         en.end_id, en.end_rua, en.end_numero, en.end_bairro, en.end_complemento, en.end_cep,
+         ct.ctt_id, ct.ctt_telefone, ct.ctt_celular, ct.ctt_email,
+         p.pj_id, p.pj_razao_social, p.pj_nome_fantasia, p.pj_cnpj`,
+      )
+      .from('pessoa_juridica p')
+      .innerJoin('contato ct')
+      .on('ct.ctt_id = p.ctt_id')
+      .innerJoin('endereco en')
+      .on('en.end_id = ct.end_id')
+      .innerJoin('cidade c')
+      .on('c.cid_id = en.cid_id')
+      .innerJoin('estado e')
+      .on('e.est_id = c.est_id');
+
+    if (fields) {
+      if (fields.id) {
+        parameters.push(fields.id);
+        builder = builder.where('p.pj_id = ?');
+      }
+    }
+
+    const query = builder.build();
+
+    const rows = await Database.instance.select(query, parameters);
+
+    for (const row of rows) {
+      persons.push(
+        new LegalPerson(
+          row.pj_id,
+          row.pj_razao_social,
+          row.pj_nome_fantasia,
+          row.pj_cnpj,
+          new Contact(
+            row.ctt_id,
+            row.ctt_telefone,
+            row.ctt_celular,
+            row.ctt_email,
+            new Address(
+              row.end_id,
+              row.end_rua,
+              row.end_numero,
+              row.end_bairro,
+              row.end_complemento,
+              row.end_cep,
+              new City(
+                row.cid_id,
+                row.cid_nome,
+                new State(row.est_id, row.est_nome, row.est_sigla),
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+
+    return persons;
   }
 }
---
+++ src/test/legal-person-model.ts
@@ -0,0 +1,65 @@
+import Database from '../util/database';
+import Contact from '../model/Contact';
+import LegalPerson from '../model/LegalPerson';
+
+const showPersons = async (): Promise<void> => {
+  await Database.instance.open();
+  const persons = await new LegalPerson().get();
+  await Database.instance.close();
+
+  for (const person of persons) {
+    console.log(person);
+  }
+};
+
+const showPersonById = async (id: number): Promise<void> => {
+  await Database.instance.open();
+  const person = (await new LegalPerson().get({ id }))[0];
+  await Database.instance.close();
+
+  console.log(person);
+};
+
+const insertPerson = async (person: LegalPerson): Promise<void> => {
+  await Database.instance.open();
+  await Database.instance.beginTransaction();
+  const result = await person.save();
+  if (result > 0) await Database.instance.commit();
+  else await Database.instance.rollback();
+  await Database.instance.close();
+
+  console.log(result);
+};
+
+const updatePerson = async (person: LegalPerson): Promise<void> => {
+  await Database.instance.open();
+  await Database.instance.beginTransaction();
+  const result = await person.save();
+  if (result > 0) await Database.instance.commit();
+  else await Database.instance.rollback();
+  await Database.instance.close();
+
+  console.log(result);
+};
+
+const deletePerson = async (id: number): Promise<void> => {
+  await Database.instance.open();
+  await Database.instance.beginTransaction();
+  const result = await new LegalPerson(id).delete();
+  if (result > 0) await Database.instance.commit();
+  else await Database.instance.rollback();
+  await Database.instance.close();
+
+  console.log(result);
+};
+
+showPersons();
+// insertPerson(
+//   new LegalPerson(0, 'Teste 2', 'Teste Testando 2', '22.222.222/0002-02', new Contact(3)),
+// );
+
+// updatePerson(
+//   new LegalPerson(1, 'Teste 1', 'Teste Testando 1', '11.111.111/0001-11', new Contact(3)),
+// );
+
+//deletePerson(2);
